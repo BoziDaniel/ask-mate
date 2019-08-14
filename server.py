efd016b3dad7478766c73daf6f63f337a33fa680
Maximilian Pal <max.pal@outlook.com>
2019-08-14 22:41:49 +0200

    View fix for list of questions

--- server.py
+++ server.py
@@ -7,18 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
+    """Show list of questions, sorted by the latest question on top"""
     question_path = "sample_data/question.csv"
-    unordered_answers = dm.get_all_data_from_file(question_path)
-    sorted_lines = dm.descending_sort_data_by_id(unordered_answers)
-    sorted_questions = []
-    for line in sorted_lines:
-        sorted_questions.append(line["message"])
+    unordered_questions = dm.get_all_data_from_file(question_path)
+    sorted_questions = dm.descending_sort_data_by_parameter(unordered_questions, 'submission_time')
+    # sorted_questions_l = []
+    # for dict in sorted_questions:
+    #     sorted_questions_l.append(dm.find_the_key(dict, "title"))
     return render_template('list.html', sorted_questions=sorted_questions)
 
-@app.route('/question/<question_id>')
-def route_expand_question(question_id: int):
+
+@app.route('/add-question.html')
+def add_question():
+    """Add new question to list, then redirect to /list page"""
+    return render_template('add-question.html')
 
 
+# @app.route('/question/<question_id>')
+# def route_expand_question(question_id: int):
+#
 
 if __name__ == '__main__':
     app.run(
