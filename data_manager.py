a15252b516c3925d703f5c15e3a892f646f803b0
Bozi Daniel <daniel.bozi88@gmail.com>
2019-08-14 15:06:36 +0200

    wonder

--- data_manager.py
+++ data_manager.py
@@ -45,19 +45,19 @@ def get_values_from_dict(dict_data):
     return values_of_a_dict
 
 
-def get_a_column_from_data(column_name, file_path):
-    data = get_all_data_from_file(file_path)  # list of dicts
+def get_a_column_from_data(column_name, list_of_dicts):
     items_in_column = []
-    for subdict in data:
+    for subdict in list_of_dicts:
         items_in_column.append(subdict[column_name])
     return items_in_column
 
 
-"""
+
 #mind2 fv müxik, ez a 2. ra próba
 x = [{"id": 112, "name": "aga"}, {"id": 214, "name": "wzew"}, {"id": 1, "name": "awrrza"}]
 y = descending_sort_data_by_id(x)
 print(y)
 z = get_values_from_dict({"id": 112, "name": "aga"})
 print(z)
-"""
+
+print(get_a_column_from_data('title', questions_path))
