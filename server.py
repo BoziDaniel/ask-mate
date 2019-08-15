caeb4ec2dd5d50f0ff5f5bb11ea272af01fed7c4
Bozi Daniel <daniel.bozi88@gmail.com>
2019-08-15 13:16:58 +0200

    debug issue

--- server.py
+++ server.py
@@ -12,10 +12,16 @@ def route_list():
     question_path = "sample_data/question.csv"
     unordered_questions = dm.get_all_data_from_file(question_path)
     sorted_questions = dm.descending_sort_data_by_parameter(unordered_questions, 'submission_time')
-    # sorted_questions_l = []
-    # for dict in sorted_questions:
-    #     sorted_questions_l.append(dm.find_the_key(dict, "title"))
     return render_template('list.html', sorted_questions=sorted_questions)
+
+
+@app.route('/question/<question_id>')
+def route_expand_question(question_id):
+    question_path = "sample_data/question.csv"
+    answer_path = "sample_data/answer.csv"
+    all_questions = dm.get_all_data_from_file(question_path)
+    all_answers = dm.get_all_data_from_file(answer_path)
+    return render_template('question.html', question_id=question_id, answers=all_answers, questions=all_questions)
 
 
 @app.route('/add-question')
@@ -24,10 +30,6 @@ def add_question():
     # cn.add_new_question()
     return render_template('add-question.html')
 
-
-# @app.route('/question/<question_id>')
-# def route_expand_question(question_id: int):
-#
 
 if __name__ == '__main__':
     app.run(
